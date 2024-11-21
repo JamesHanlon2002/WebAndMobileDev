fa641a74061f8dc63af71b12ca38ec6e7841c524
JamesMHanlon <jmhanlon2002@gmail.com>
2024-11-21 14:27:24 +0000

    removed routes

--- routes/home.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.render('home'); // Assuming 'home.ejs' exists in the 'views' folder
-});
-
-module.exports = router;
